784fbbe25e315c394b35dc6eb1f613b010d71e4e
Stephen Passero <stephen.passero@gmail.com>
2018-05-26 11:45:07 -0400

    Make the program return 'tie' when the round is a tie

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,22 +1,28 @@
 class RPS
-  def wins?(object, otherObject)
+  def wins(object, otherObject)
     if object == "rock"
       if otherObject == "scissors"
         return true
       elsif otherObject == "paper"
         return false;
+      else
+        return "tie"
       end
     elsif object == "paper"
       if otherObject == "rock"
         return true
       elsif otherObject == "scissors"
         return false;
+      else
+        return "tie"
       end
     else
       if otherObject == "paper"
         return true
       elsif otherObject == "rock"
         return false
+      else
+        return "tie"
       end
     end
   end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -4,30 +4,45 @@ require('rspec')
 describe("#rock_paper_scissors") do
   it("should return true when rock is the object and scissors is the argument") do
     game = RPS.new()
-    expect(game.wins?("rock", "scissors")).to eq(true)
+    expect(game.wins("rock", "scissors")).to eq(true)
   end
   it("should return true when paper is the object and rock is the argument") do
     game = RPS.new()
-    expect(game.wins?("paper", "rock")).to eq(true)
+    expect(game.wins("paper", "rock")).to eq(true)
   end
 
   it("should return true when scissors is the object and paper is the argument") do
     game = RPS.new()
-    expect(game.wins?("scissors", "paper")).to eq(true)
+    expect(game.wins("scissors", "paper")).to eq(true)
   end
 
   it("should return false when rock is the object and paper is the argument") do
     game = RPS.new()
-    expect(game.wins?("rock", "paper")).to eq(false)
+    expect(game.wins("rock", "paper")).to eq(false)
   end
 
   it("should return false when paper is the object and scissors is the argument") do
     game = RPS.new()
-    expect(game.wins?("paper", "scissors")).to eq(false)
+    expect(game.wins("paper", "scissors")).to eq(false)
   end
 
   it("should return false when scissors is the object and rock is the argument") do
     game = RPS.new()
-    expect(game.wins?("scissors", "rock")).to eq(false)
+    expect(game.wins("scissors", "rock")).to eq(false)
+  end
+
+  it("should return 'tie' when rock is the object and rock is the argument") do
+    game = RPS.new()
+    expect(game.wins("rock", "rock")).to eq("tie")
+  end
+
+  it("should return 'tie' when paper is the object and paper is the argument") do
+    game = RPS.new()
+    expect(game.wins("paper", "paper")).to eq("tie")
+  end
+
+  it("should return 'tie' when scissors is the object and scissors is the argument") do
+    game = RPS.new()
+    expect(game.wins("scissors", "scissors")).to eq("tie")
   end
 end
