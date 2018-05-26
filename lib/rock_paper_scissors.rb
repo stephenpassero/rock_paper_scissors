09fa819e5f349b59cb7e82d5d039206e6d554525
Stephen Passero <stephen.passero@gmail.com>
2018-05-26 11:35:10 -0400

    Improve functionality to return false when the player loses the round

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -3,14 +3,20 @@ class RPS
     if object == "rock"
       if otherObject == "scissors"
         return true
+      elsif otherObject == "paper"
+        return false;
       end
     elsif object == "paper"
       if otherObject == "rock"
         return true
+      elsif otherObject == "scissors"
+        return false;
       end
     else
       if otherObject == "paper"
         return true
+      elsif otherObject == "rock"
+        return false
       end
     end
   end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -2,18 +2,32 @@ require('rock_paper_scissors')
 require('rspec')
 
 describe("#rock_paper_scissors") do
-  it("should return true if rock is the object and scissors is the argument") do
+  it("should return true when rock is the object and scissors is the argument") do
     game = RPS.new()
     expect(game.wins?("rock", "scissors")).to eq(true)
   end
-  it("should return true if paper is the object and rock is the argument") do
+  it("should return true when paper is the object and rock is the argument") do
     game = RPS.new()
     expect(game.wins?("paper", "rock")).to eq(true)
   end
 
-  it("should return true if scissors is the object and paper is the argument") do
+  it("should return true when scissors is the object and paper is the argument") do
     game = RPS.new()
     expect(game.wins?("scissors", "paper")).to eq(true)
   end
 
+  it("should return false when rock is the object and paper is the argument") do
+    game = RPS.new()
+    expect(game.wins?("rock", "paper")).to eq(false)
+  end
+
+  it("should return false when paper is the object and scissors is the argument") do
+    game = RPS.new()
+    expect(game.wins?("paper", "scissors")).to eq(false)
+  end
+
+  it("should return false when scissors is the object and rock is the argument") do
+    game = RPS.new()
+    expect(game.wins?("scissors", "rock")).to eq(false)
+  end
 end
